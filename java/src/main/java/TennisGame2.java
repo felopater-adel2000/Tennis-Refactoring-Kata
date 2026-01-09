f70345b82e994a1baff7bf8494fb8cbd5e40da14
felopater-adel12 <felopater.sadek@maxab.io>
2026-01-09 17:47:12 +0200

    Refactor score calculation logic in TennisGame2

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -14,17 +14,81 @@ public class TennisGame2 implements TennisGame {
     }
 
     public String getScore() {
-        if(P1point == P2point) {
-            return dummy();
-        }
+        if(P1point == P2point) return handleEqualityScore();
         else {
-            return dummy();
+            String score = "";
+
+            if (P1point > 0 && P2point == 0) {
+                if (P1point == 1)
+                    P1res = "Fifteen";
+                if (P1point == 2)
+                    P1res = "Thirty";
+                if (P1point == 3)
+                    P1res = "Forty";
+
+                P2res = "Love";
+                score = P1res + "-" + P2res;
+            }
+
+            if (P2point > 0 && P1point == 0) {
+                if (P2point == 1)
+                    P2res = "Fifteen";
+                if (P2point == 2)
+                    P2res = "Thirty";
+                if (P2point == 3)
+                    P2res = "Forty";
+
+                P1res = "Love";
+                score = P1res + "-" + P2res;
+            }
+
+            if (P1point > P2point && P1point < 4) {
+                if (P1point == 2)
+                    P1res = "Thirty";
+                if (P1point == 3)
+                    P1res = "Forty";
+                if (P2point == 1)
+                    P2res = "Fifteen";
+                if (P2point == 2)
+                    P2res = "Thirty";
+                score = P1res + "-" + P2res;
+            }
+
+            if (P2point > P1point && P2point < 4) {
+                if (P2point == 2)
+                    P2res = "Thirty";
+                if (P2point == 3)
+                    P2res = "Forty";
+                if (P1point == 1)
+                    P1res = "Fifteen";
+                if (P1point == 2)
+                    P1res = "Thirty";
+                score = P1res + "-" + P2res;
+            }
+
+            if (P1point > P2point && P2point >= 3) {
+                score = "Advantage player1";
+            }
+
+            if (P2point > P1point && P1point >= 3) {
+                score = "Advantage player2";
+            }
+
+            if (P1point >= 4 && P2point >= 0 && (P1point - P2point) >= 2) {
+                score = "Win for player1";
+            }
+
+            if (P2point >= 4 && P1point >= 0 && (P2point - P1point) >= 2) {
+                score = "Win for player2";
+            }
+            return score;
         }
     }
 
-    private String dummy() {
+    private String handleEqualityScore() {
         String score = "";
-        if (P1point == P2point && P1point < 4) {
+
+        if (P1point < 4) {
             if (P1point == 0)
                 score = "Love";
             if (P1point == 1)
@@ -34,73 +98,10 @@ public class TennisGame2 implements TennisGame {
             score += "-All";
         }
 
-        if (P1point == P2point && P1point >= 3) {
+        if (P1point >= 3) {
             score = "Deuce";
         }
 
-        if (P1point > 0 && P2point == 0) {
-            if (P1point == 1)
-                P1res = "Fifteen";
-            if (P1point == 2)
-                P1res = "Thirty";
-            if (P1point == 3)
-                P1res = "Forty";
-
-            P2res = "Love";
-            score = P1res + "-" + P2res;
-        }
-
-        if (P2point > 0 && P1point == 0) {
-            if (P2point == 1)
-                P2res = "Fifteen";
-            if (P2point == 2)
-                P2res = "Thirty";
-            if (P2point == 3)
-                P2res = "Forty";
-
-            P1res = "Love";
-            score = P1res + "-" + P2res;
-        }
-
-        if (P1point > P2point && P1point < 4) {
-            if (P1point == 2)
-                P1res = "Thirty";
-            if (P1point == 3)
-                P1res = "Forty";
-            if (P2point == 1)
-                P2res = "Fifteen";
-            if (P2point == 2)
-                P2res = "Thirty";
-            score = P1res + "-" + P2res;
-        }
-
-        if (P2point > P1point && P2point < 4) {
-            if (P2point == 2)
-                P2res = "Thirty";
-            if (P2point == 3)
-                P2res = "Forty";
-            if (P1point == 1)
-                P1res = "Fifteen";
-            if (P1point == 2)
-                P1res = "Thirty";
-            score = P1res + "-" + P2res;
-        }
-
-        if (P1point > P2point && P2point >= 3) {
-            score = "Advantage player1";
-        }
-
-        if (P2point > P1point && P1point >= 3) {
-            score = "Advantage player2";
-        }
-
-        if (P1point >= 4 && P2point >= 0 && (P1point - P2point) >= 2) {
-            score = "Win for player1";
-        }
-
-        if (P2point >= 4 && P1point >= 0 && (P2point - P1point) >= 2) {
-            score = "Win for player2";
-        }
         return score;
     }
 
