f87c97a7a36c989bb6a1b5b7f71be29098e81edc
felopater-adel12 <felopater.sadek@maxab.io>
2026-01-11 17:39:41 +0200

    Refactor score calculation logic in TennisGame2 for improved clarity and structure

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -90,60 +90,57 @@ public class TennisGame2 implements TennisGame {
 
             return score;
         }
+        else if(P1point > P2point) {
+                String score = "";
+
+                if (P1point < 4) {
+                    if (P1point == 2)
+                        P1res = "Thirty";
+                    if (P1point == 3)
+                        P1res = "Forty";
+                    if (P2point == 1)
+                        P2res = "Fifteen";
+                    if (P2point == 2)
+                        P2res = "Thirty";
+                    score = P1res + "-" + P2res;
+                }
+
+                if (P2point >= 3) {
+                    score = "Advantage player1";
+                }
+
+                if (P1point >= 4 && P2point >= 0 && (P1point - P2point) >= 2) {
+                    score = "Win for player1";
+                }
+
+                return score;
+            }
         else {
-            if(P1point > P2point) {
-                return dummy();
+                String score = "";
+
+                if (P2point < 4) {
+                    if (P2point == 2)
+                        P2res = "Thirty";
+                    if (P2point == 3)
+                        P2res = "Forty";
+                    if (P1point == 1)
+                        P1res = "Fifteen";
+                    if (P1point == 2)
+                        P1res = "Thirty";
+                    score = P1res + "-" + P2res;
+                }
+
+                if (P1point >= 3) {
+                    score = "Advantage player2";
+                }
+
+                if (P2point >= 4 && P1point >= 0 && (P2point - P1point) >= 2) {
+                    score = "Win for player2";
+                }
+                return score;
             }
-            else {
-                return dummy();
-            }
-        }
 
-    }
 
-    private String dummy() {
-        String score = "";
-
-        if (P1point > P2point && P1point < 4) {
-            if (P1point == 2)
-                P1res = "Thirty";
-            if (P1point == 3)
-                P1res = "Forty";
-            if (P2point == 1)
-                P2res = "Fifteen";
-            if (P2point == 2)
-                P2res = "Thirty";
-            score = P1res + "-" + P2res;
-        }
-
-        if (P2point > P1point && P2point < 4) {
-            if (P2point == 2)
-                P2res = "Thirty";
-            if (P2point == 3)
-                P2res = "Forty";
-            if (P1point == 1)
-                P1res = "Fifteen";
-            if (P1point == 2)
-                P1res = "Thirty";
-            score = P1res + "-" + P2res;
-        }
-
-        if (P1point > P2point && P2point >= 3) {
-            score = "Advantage player1";
-        }
-
-        if (P2point > P1point && P1point >= 3) {
-            score = "Advantage player2";
-        }
-
-        if (P1point >= 4 && P2point >= 0 && (P1point - P2point) >= 2) {
-            score = "Win for player1";
-        }
-
-        if (P2point >= 4 && P1point >= 0 && (P2point - P1point) >= 2) {
-            score = "Win for player2";
-        }
-        return score;
     }
 
     public void P1Score() {
