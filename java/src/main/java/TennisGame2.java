2aab2d3e8e6f9b3c1323f8513282cb00dcda0207
felopater-adel12 <felopater.sadek@maxab.io>
2026-01-12 11:31:13 +0200

    Refactor TennisGame2 to simplify score assignment logic for improved clarity

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -62,19 +62,17 @@ public class TennisGame2 implements TennisGame {
         else if (P2point == 0) {
             String score = "";
 
-            if (P1point > 0) {
-                if (P1point == 1)
-                    P1res = "Fifteen";
-                if (P1point == 2)
-                    P1res = "Thirty";
-                if (P1point == 3)
-                    P1res = "Forty";
+            if (P1point == 1)
+                P1res = "Fifteen";
+            if (P1point == 2)
+                P1res = "Thirty";
+            if (P1point == 3)
+                P1res = "Forty";
 
-                P2res = "Love";
-                score = P1res + "-" + P2res;
-            }
+            P2res = "Love";
+            score = P1res + "-" + P2res;
 
-            if (P1point > P2point && P1point < 4) {
+            if (P1point < 4) {
                 if (P1point == 2)
                     P1res = "Thirty";
                 if (P1point == 3)
